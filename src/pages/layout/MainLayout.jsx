ce89c0cbb066289e8b97bf3289b0a8a9a26e9711
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-14 22:03:58 +0600

    layout update

--- src/pages/layout/MainLayout.jsx
+++ src/pages/layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import UpperNav from './UpperNav'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 const MainLayout = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
         <UpperNav/>
         <NavBar/>
         <Outlet/>
+        <Footer/>
     </>
   )
 }
