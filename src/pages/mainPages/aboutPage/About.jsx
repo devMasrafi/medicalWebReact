01a668394e3bd517e2cc4f1e8a84c3507757ffad
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-19 10:06:55 +0600

    about page responsive done

--- src/pages/mainPages/aboutPage/About.jsx
+++ src/pages/mainPages/aboutPage/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       {/* hero section */}
       <section className="relative bg-aboutHeroImg bg-no-repeat bg-cover h-[350px] ">
         <div className="absolute inset-0 bg-white/50">
-          <div className="container mx-auto pt-[12rem] ">
+          <div className="container mx-auto pt-[12rem] sm:px-2 md:px-3 ">
             <div className="font-workSans shadow-md ">
               <a href="/" className="hover:text-AscColor transition-colors">
                 Home
@@ -22,23 +22,24 @@ const About = () => {
       </section>
       {/* About welcome section */}
       <section className="container mx-auto mt-[4rem]">
-        <div>
+        <div className="sm:p-2 sm:text-sm 2xl:text-base">
           <div className="flex justify-evenly ">
             <div>
               <img
+                className="sm:hidden 2xl:block"
                 src="./images/aboutWelcomeImg.png"
                 alt="about welcome image"
               />
             </div>
             <div>
               <div>
-                <h3 className="text-xl uppercase text-secColor font-workSans font-medium ">
+                <h3 className="md:text-xl uppercase text-secColor font-workSans font-medium ">
                   Welcome to Hospital name
                 </h3>
-                <h2 className="font-yesevaOne text-3xl capitalize text-mainColor   ">
+                <h2 className="font-yesevaOne md:text-3xl sm:text-xl sm:w-[12rem]  capitalize text-mainColor   ">
                   Best Care for Your Good Health
                 </h2>
-                <div className="flex py-5 gap-12 ">
+                <div className="2xl:flex py-5 gap-12 ">
                   <ul className="list-disc list-inside marker:text-blue-600">
                     <li>A Passion for Healing</li>
                     <li>All our best</li>
@@ -50,7 +51,7 @@ const About = () => {
                     <li>Always Caring</li>
                   </ul>
                 </div>
-                <div className="py-8 w-[564px] ">
+                <div className="py-8 md:w-[564px] ">
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Quisque placerat scelerisque tortor ornare ornare. Quisque
@@ -58,7 +59,7 @@ const About = () => {
                     vitae tortor augue. Velit nascetur proin massa in. Consequat
                     faucibus porttitor enim et.
                   </p>
-                  <p className="py-6">
+                  <p className="py-6 sm:hidden md:block">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Quisque placerat scelerisque. Convallis felis vitae tortor
                     augue. Velit nascetur proin massa in.
@@ -74,7 +75,7 @@ const About = () => {
       <section className="relative bg-aboutMessageBg bg-cover h-[440px] my-14 text-white ">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-900/80 to-black/70">
           <div className="text-center flex flex-col items-center justify-center">
-            <div className="w-[554px] flex flex-col items-center mt-[5rem] ">
+            <div className="md:w-[554px] flex flex-col items-center mt-[5rem] ">
               <img
                 className="w-[4rem]"
                 src="./images/quotionVector.svg"
@@ -97,8 +98,8 @@ const About = () => {
       <section className="font-workSans">
         <div className="container mx-auto px-4 py-8">
           <h2 className="text-3xl font-bold text-center mb-8">Our Doctors</h2>
-          <div className="flex justify-center gap-8">
-            <div className="border rounded-md ">
+          <div className="md:flex justify-center gap-8">
+            <div className="border rounded-md my-2">
               <div className="text-center">
                 <img src="./images/doctorimage-1.jpg" alt="" />
                 <div className="py-4">
@@ -112,29 +113,33 @@ const About = () => {
                 </h4>
               </div>
             </div>
-            <div className="text-center">
-              <img src="./images/doctorimage-2.png" alt="" />
-              <div className="py-4">
-                <h3 className="capitalize ">doctor name</h3>
-                <h2 className="uppercase font-medium text-secColor  ">
-                  neurology
-                </h2>
+            <div className="border rounded-md my-2">
+              <div className="text-center">
+                <img src="./images/doctorimage-2.png" alt="" />
+                <div className="py-4">
+                  <h3 className="capitalize ">doctor name</h3>
+                  <h2 className="uppercase font-medium text-secColor  ">
+                    neurology
+                  </h2>
+                </div>
+                <h4 className="bg-mainColor w-full py-2 text-white uppercase">
+                  view profile
+                </h4>
               </div>
-              <h4 className="bg-mainColor w-full py-2 text-white uppercase">
-                view profile
-              </h4>
             </div>
-            <div className="text-center">
-              <img src="./images/doctorimage-3.jpg" alt="" />
-              <div className="py-4">
-                <h3 className="capitalize ">doctor name</h3>
-                <h2 className="uppercase font-medium text-secColor  ">
-                  neurology
-                </h2>
+            <div className="border rounded-md my-2">
+              <div className="text-center">
+                <img src="./images/doctorimage-3.jpg" alt="" />
+                <div className="py-4">
+                  <h3 className="capitalize ">doctor name</h3>
+                  <h2 className="uppercase font-medium text-secColor  ">
+                    neurology
+                  </h2>
+                </div>
+                <h4 className="bg-mainColor w-full py-2 text-white uppercase">
+                  view profile
+                </h4>
               </div>
-              <h4 className="bg-mainColor w-full py-2 text-white uppercase">
-                view profile
-              </h4>
             </div>
           </div>
         </div>
@@ -143,36 +148,47 @@ const About = () => {
       <section className="container mx-auto py-10">
         <div>
           <div className="text-center my-20 ">
-            <h3 className="text-lg uppercase font-semibold text-secColor">
+            <h3 className="md:text-lg sm:text-xs uppercase font-semibold text-secColor">
               get in touch
             </h3>
             <h2 className="font-yesevaOne text-3xl pt-2">contact</h2>
           </div>
-          <div className="flex justify-evenly items-center">
-            <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
+          <div className="md:flex justify-evenly items-center sm:flex sm:flex-wrap sm:gap-4">
+            <div className=" md:w-[230px] md:h-[230px] md:gap-2 bg-AscColor flex flex-col justify-center items-center rounded-md sm:w-[8rem] sm:h-[8rem] sm:text-sm sm:p-2 ">
               <div>
-                <h3 className="text-lg uppercase font-medium ">emergency</h3>
+                <h3 className="md:text-lg sm:text-xs uppercase font-medium ">
+                  emergency
+                </h3>
                 <p>(237) 681-812-255</p>
                 <p>(237) 666-331-894</p>
               </div>
             </div>
-            <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
+            <div className=" md:w-[230px] md:h-[230px] md:gap-2 bg-AscColor flex flex-col justify-center items-center rounded-md sm:w-[8rem] sm:h-[8rem] sm:text-sm sm:p-2 ">
               <div>
-                <h3 className="text-lg uppercase font-medium ">Location</h3>
+                <h3 className="md:text-lg sm:text-xs uppercase font-medium ">
+                  Location
+                </h3>
                 <p>0123 Some place</p>
                 <p>9876 Some country</p>
               </div>
             </div>
-            <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
+            <div className=" md:w-[230px] md:h-[230px] md:gap-2 bg-AscColor flex flex-col justify-center items-center rounded-md sm:w-[8rem] sm:h-[8rem] sm:text-sm sm:p-2 ">
               <div>
-                <h3 className="text-lg uppercase font-medium ">Email</h3>
-                <p>fildineeesoe@gmil.com</p>
-                <p>myebstudios@gmail.com</p>
+                <h3 className="md:text-lg sm:text-xs uppercase font-medium ">
+                  Email
+                </h3>
+                <p className="sm:block md:hidden">
+                  fildinee
+                  <br />
+                  esoe@gmil.com
+                </p>
+                <p className="sm:hidden md:block">fildineeesoe@gmil.com</p>
+                <p className="sm:hidden md:block">myebstudios@gmail.com</p>
               </div>
             </div>
-            <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
+            <div className=" md:w-[230px] md:h-[230px] md:gap-2 bg-AscColor flex flex-col justify-center items-center rounded-md sm:w-[8rem] sm:h-[8rem] sm:text-sm sm:p-2 ">
               <div>
-                <h3 className="text-lg uppercase font-medium ">
+                <h3 className="md:text-lg uppercase font-medium ">
                   Working Hours
                 </h3>
                 <p>Mon-Sat 09:00-20:00</p>
