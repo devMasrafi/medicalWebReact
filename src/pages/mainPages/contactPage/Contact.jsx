85e60bcaf828c8809729a89c337e3bb3ce27e6e8
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-19 11:57:28 +0600

    contact page done

--- src/pages/mainPages/contactPage/Contact.jsx
+++ src/pages/mainPages/contactPage/Contact.jsx
@@ -31,11 +31,11 @@ const Contact = () => {
       <section className="container mx-auto">
         <div className="my-8 flex flex-col items-center justify-center">
           <img
-            className="rounded-md border-mainColor border-2"
+            className="rounded-md border-mainColor border-2 2xl:w-[70rem] md:w-[35rem] sm:w-[18rem] "
             src="./images/contactImages/Mapview.png"
             alt="google images"
           />
-          <p>
+          <p className="md:w-[35rem] sm:w-[18rem] sm:text-center md:text-center ">
             Due to not having a google api or using google cloud Map AIP this
             will be static
           </p>
@@ -44,24 +44,24 @@ const Contact = () => {
 
       {/* forum */}
       <section className="container mx-auto">
-        <div className="text-center">
+        <div className="text-center sm:w-[18rem] ">
           <h3 className="text-lg uppercase font-semibold text-secColor">
             get in touch
           </h3>
           <h2 className="font-yesevaOne text-3xl pt-2">contact</h2>
         </div>
         <div className="my-8">
-          <div className="flex justify-evenly">
+          <div className="2xl:flex justify-evenly">
             <div>
               <form
                 onSubmit={onSubmitHandler}
                 action="submit"
-                className="bg-mainColor text-white rounded-lg "
+                className="bg-mainColor text-white rounded-lg sm:w-[18rem] sm:mx-auto md:w-[36rem] "
               >
-                <div className="flex flex-col ">
-                  <div className="flex justify-between items-center">
+                <div className="md:flex flex-col ">
+                  <div className="md:flex justify-between items-center ">
                     <input
-                      className="bg-mainColor text-white p-2 w-[240px] outline-none  border-r "
+                      className="bg-mainColor text-white p-2 2xl:w-[20rem] outline-none md:border-r sm:border-b md:w-[22rem] sm:w-full "
                       type="text"
                       placeholder="Name"
                       name="UserName"
@@ -69,7 +69,7 @@ const Contact = () => {
                       onChange={onChangeHandler}
                     />
                     <input
-                      className="bg-mainColor text-white p-2 w-[240px] outline-none "
+                      className="bg-mainColor text-white p-2 2xl:w-[20rem] outline-none md:w-[22rem] sm:w-full "
                       type="email"
                       placeholder="Email"
                       name="UserEmail"
@@ -78,7 +78,7 @@ const Contact = () => {
                     />
                   </div>
                   <input
-                    className="bg-mainColor text-white p-2 outline-none border-t"
+                    className="bg-mainColor text-white p-2 outline-none border-t sm:w-full"
                     type="text"
                     name="UserSubject"
                     value={formInput.UserSubject}
@@ -86,23 +86,23 @@ const Contact = () => {
                     placeholder="Subject"
                   />
                   <textarea
-                    className="bg-mainColor text-white p-2 outline-none border-y"
+                    className="bg-mainColor text-white p-2 outline-none border-y sm:w-full"
                     name="UserMessage"
                     value={formInput.UserMessage}
                     onChange={onChangeHandler}
                     placeholder="Message"
                     rows={9}
                   ></textarea>
-                  <button className="bg-secColor py-3 font-medium capitalize text-lg ">
+                  <button className="bg-secColor py-3 font-medium capitalize 2xl:text-lg sm:w-full ">
                     submit
                   </button>
                 </div>
               </form>
             </div>
-            <div className="w-[585px] ">
+            <div className="2xl:w-[35rem] 2xl:mt-0 md:w-[32rem] md:mx-auto md:mt-[4rem] sm:w-[18rem] sm:mx-auto sm:mt-8 ">
               <div className="flex flex-wrap items-center gap-8">
-                <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
-                  <div>
+                <div className=" md:w-[230px] md:h-[230px] bg-mainColor text-white flex flex-col justify-center rounded-md sm:w-full sm:h-[6rem]">
+                  <div className="2xl:w-[10rem] mx-auto">
                     <h3 className="text-lg uppercase font-medium ">
                       emergency
                     </h3>
@@ -110,22 +110,22 @@ const Contact = () => {
                     <p>(237) 666-331-894</p>
                   </div>
                 </div>
-                <div className=" w-[230px] h-[230px] bg-mainColor text-white flex flex-col justify-center items-center rounded-md ">
-                  <div>
+                <div className=" md:w-[230px] md:h-[230px] bg-mainColor text-white flex flex-col justify-center rounded-md sm:w-full sm:h-[6rem]">
+                  <div className="2xl:w-[10rem] mx-auto">
                     <h3 className="text-lg uppercase font-medium ">Location</h3>
                     <p>0123 Some place</p>
                     <p>9876 Some country</p>
                   </div>
                 </div>
-                <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
-                  <div>
+                <div className=" md:w-[230px] md:h-[230px] bg-mainColor text-white flex flex-col justify-center rounded-md sm:w-full sm:h-[6rem]">
+                  <div className="2xl:w-[10rem] mx-auto">
                     <h3 className="text-lg uppercase font-medium ">Email</h3>
                     <p>fildineeesoe@gmil.com</p>
-                    <p>myebstudios@gmail.com</p>
+                    <p>myebstudi<br />os@gmail.com</p>
                   </div>
                 </div>
-                <div className=" w-[230px] h-[230px] bg-AscColor flex flex-col justify-center items-center rounded-md ">
-                  <div>
+                <div className=" md:w-[230px] md:h-[230px] bg-mainColor text-white flex flex-col justify-center rounded-md sm:w-full sm:h-[6rem]">
+                  <div className="2xl:w-[10rem] mx-auto">
                     <h3 className="text-lg uppercase font-medium ">
                       Working Hours
                     </h3>
@@ -143,7 +143,7 @@ const Contact = () => {
         <div className="text-center text-3xl font-yesevaOne capitalize" >
           <h2>recent news</h2>
         </div>
-        <div className=" my-14">
+        <div className="my-14 sm:w-[19rem] md:w-full ">
           <div className="flex flex-wrap gap-5 justify-evenly">
             <div className="rounded-md flex gap-4 w-[585px] border  ">
               <div>
